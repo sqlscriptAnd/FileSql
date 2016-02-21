6f50ef8e7715466d0686952102246da139fdf832
arm <soceityarmkung@hotmail.com>
2016-02-21 16:16:41 +0700

    test

--- Insert/ADMIN_INSERT.sql
+++ Insert/ADMIN_INSERT.sql
@@ -1,3 +1,7 @@
 INSERT INTO ADMIN (USERNAME,PWD,FNAME,LNAME,POSITION)
 VALUES ('admin2','admin6','Nattachai','Summart','Customer');
 
+INSERT INTO ADMIN (USERNAME,PWD,FNAME,LNAME,POSITION)
+VALUES ('admin6','admin4','Nattachai','Summart','Customer');
+
+
